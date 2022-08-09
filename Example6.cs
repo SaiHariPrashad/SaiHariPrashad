2c7fe62d63ff861d5077b8060183df716b69e390
SaiHariPrashad <97112681+SaiHariPrashad@users.noreply.github.com>
2022-08-09 09:31:11 +0530

    Add files via upload

---
+++ Example6.cs
@@ -0,0 +1,26 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example6 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter the Number :");
+            int a = Convert.ToInt32(Console.ReadLine());
+            if ((a % 2) ==0)
+                Console.WriteLine("The Given Number is Even", a);
+            else
+                Console.WriteLine("The Given Number is Odd", a);
+
+            
+        }
+        
+        
+    }
+}*/
